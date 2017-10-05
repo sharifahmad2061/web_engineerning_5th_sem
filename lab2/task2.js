b21d29fb6e3edec4d9b36ca5609f9abf2971bf46
Sharif Ahmad <sharifahmad2061@gmail.com>
2017-10-05 13:30:04 +0500

    do lab 3

---
+++ lab2/task2.js
@@ -0,0 +1,5 @@
+arr={
+    design:"https://en.wikipedia.org/wiki/Web_design",
+    history: "https://en.wikipedia.org/wiki/History_of_the_World_Wide_Web",
+    crawl: "https://en.wikipedia.org/wiki/Web_crawler"
+};
