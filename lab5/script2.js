6991eb1003dd3766bf6f3d3776be5ee5f2e4c6e6
sharifahmad <sharifahmad2061@gmail.com>
2017-10-19 22:12:02 +0500

    finalize task 2

--- lab5/script2.js
+++ lab5/script2.js
@@ -8,23 +8,35 @@ var digit = new RegExp(/[0-9]/);
 var punctuation = new RegExp(/[.,\/#!$%\^&\*;:{}=\-_`~()]/);
 
 function strength(){
-    // p.innerHTML = pass.value;
     var dest = pass.value;
     temp = 0;
+    if (dest.length > 7) temp += 1;
     res = small.exec(dest);
-    temp += (res != "") ? 1 : 0;
+    temp += ((res === null) ? 0 : 1);
     res = "";
     res = capital.exec(dest);
-    temp += (res != "") ? 1 : 0;
+    temp += ((res === null) ? 0 : 1);
     res = "";
     res = digit.exec(dest);
-    temp += (res != "") ? 1 : 0;
+    temp += ((res === null) ? 0 : 1);
     res = "";
     res = punctuation.exec(dest);
-    temp += (res != "") ? 1 : 0;
-    res = "";
-    if (pass.value.length > 7) temp += 1;
+    temp += ((res === null) ? 0 : 1);
     p.innerHTML = (temp <= 2) ? "poor" : (temp == 3) ? "bad" : (temp == 4) ? "good" : "strong";
+    switch (p.innerHTML) {
+        case "poor":
+            pass.style.backgroundColor = "#bdbdbd";
+            break;
+        case "bad":
+            pass.style.backgroundColor = "#ffeb3b";
+            break;
+        case "good":
+            pass.style.backgroundColor = "#c6ff00";
+            break;
+        case "strong":
+            pass.style.backgroundColor = "#69f0ae";
+            break;
+    }
 }
 
 pass.addEventListener("keypress",function(code){
