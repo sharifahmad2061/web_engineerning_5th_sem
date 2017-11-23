29766015ef1ebc1d2497f20ba81cca814fba9ef8
sharifahmad <sharifahmad2061@gmail.com>
2017-11-24 00:09:11 +0500

    finalizes the lab9

--- lab9/script.js
+++ lab9/script.js
@@ -1,20 +1,26 @@
 $(document).ready(function(){
     
-      $("#Isd").change(function(){
+    $("#Isd").change(function(){
+        $("#cards > div").show();
         if($(this).prop("checked") == true){
             $(" img[name |= 'Lahore'] ").hide();
         }else{
             $(" img[name |= 'Lahore'] ").show();
         }
-      });
+    });
     
-      $("#Lhr").change(function(){
-          if($(this).prop("checked") == true){
+    $("#Lhr").change(function(){
+        $("#cards > div").show();
+        if($(this).prop("checked") == true){
             $(" img[name |= 'Islamabad'] ").hide();
         }else{
             $(" img[name |= 'Islamabad'] ").show();
         }
-      });
+    });
+      
+    $("div").click(function(){
+        $(this).toggle("slow");
+    });
     
-    });	/*----ready function------*/
+});	/*----ready function------*/
     
